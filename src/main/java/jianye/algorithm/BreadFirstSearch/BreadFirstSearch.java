46cc3626a8293f913e7354c19590308c46235fa2
jianye <jianye168@gmail.com>
2016-08-17 15:45:55 -0700

    BFS update

--- src/main/java/jianye/algorithm/BreadFirstSearch/BreadFirstSearch.java
+++ src/main/java/jianye/algorithm/BreadFirstSearch/BreadFirstSearch.java
@@ -18,7 +18,16 @@ solution:
     improved method:
 
     time and space complexity:
+        The time complexity can be expressed as {\displaystyle O(|V|+|E|)} O(|V|+|E|),[5] since every vertex and every edge
+        will be explored in the worst case. {\displaystyle |V|} |V| is the number of vertices and {\displaystyle |E|} |E|
+        is the number of edges in the graph. Note that {\displaystyle O(|E|)} O(|E|) may vary between {\displaystyle O(1)}
+        O(1) and {\displaystyle O(|V|^{2})} O(|V|^{2}), depending on how sparse the input graph is.
 
+        When the number of vertices in the graph is known ahead of time, and additional data structures are used to determine
+        which vertices have already been added to the queue, the space complexity can be expressed as {\displaystyle O(|V|)} O(|V|),
+        where {\displaystyle |V|} |V| is the cardinality of the set of vertices (as said before). If the graph is represented by an
+        adjacency list it occupies {\displaystyle \Theta (|V|+|E|)} \Theta (|V|+|E|)[6] space in memory, while an adjacency matrix
+        representation occupies {\displaystyle \Theta (|V|^{2})} \Theta (|V|^{2}).[7]
     test with examples:
 
     follow up:
@@ -50,11 +59,14 @@ public class BreadFirstSearch {
         Set<UndirectedGraphNode> visited = new HashSet<>();
         visited.add(node);
         while (!queue.isEmpty()) {
+            //TODO: Remind, the dequeued node has already been visited before enqueueing
             UndirectedGraphNode poped = queue.poll();
             for (UndirectedGraphNode neighbor : poped.neighbors) {
+                //TODO: REMIND, because it is a graph, check if the neighbor node has been visited, to avoid pushing duplicated nodes back to the queue.
                 if (!visited.contains(neighbor)) {
                     visit(neighbor);
                     visited.add(neighbor);
+                    //TODO: REMIND, The node pushed into the queue must have been visited
                     queue.add(neighbor);
                 } else {
                     System.out.println("node " + poped.label + " has been visited!");
